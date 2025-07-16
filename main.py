07d9d2597eaf612018ed326b2ec8dc31cdc5f463
Avei20 <muhammadabdulazizalghofari@gmail.com>
2025-07-16 08:49:53 +0700

    Initialize OpenLit only if OTLP endpoint is set
    Use DB_URL from environment variable with fallback
    
    Add print statements for DB session initialization status
    
    Handle DB session initialization failure without exception details

--- main.py
+++ main.py
@@ -3,24 +3,31 @@ import openlit
 import os
 from google.adk.cli.fast_api import get_fast_api_app
 
-# openlit.init(
-#     otlp_endpoint=os.getenv("otel-collector", "http://localhost:4318"),
-#     application_name="calendar_agent",
-#     disable_batch=True,
-#     environment="development",
-# )
+otlp_endpoint = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT", "")
 
-DB_URL = "postgresql://postgres:postgres@localhost:5432/adk"
+# Initialize Instrumentation if otlp_endpoint is not empty
+if otlp_endpoint != "" :
+    openlit.init(
+        otlp_endpoint=otlp_endpoint,
+        application_name="calendar_agent",
+        disable_batch=True,
+        environment="development",
+    )
+
+DB_URL = os.getenv("DB_URL", "postgresql://postgres:postgres@localhost:5432/adk")
+
 # session_service = DatabaseSessionService(db_url=DB_URL)
 # Running the service with db if not use in memory
 try:
+    print("Initialize with DB Session Service")
     app = get_fast_api_app(
         session_service_uri=DB_URL,
         agents_dir=os.path.dirname(os.path.abspath(__file__)) + "/agents",
         allow_origins=["*"],
         web=True,
     )
-except Exception as e:
+except:
+    print("Failed to initalize with DB Session, Using in Memory")
     app = get_fast_api_app(
         agents_dir=os.path.dirname(os.path.abspath(__file__)) + "/agents",
         allow_origins=["*"],
