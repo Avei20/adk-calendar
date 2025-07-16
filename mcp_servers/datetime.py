a7511bcd04761e4468242ed46f5dffcb132b1fe1
Avei20 <muhammadabdulazizalghofari@gmail.com>
2025-07-16 20:45:02 +0700

    Add mcp-datetime dependency and update datetime server params

--- mcp_servers/datetime.py
+++ mcp_servers/datetime.py
@@ -1,10 +1,13 @@
+from google.adk.tools.mcp_tool.mcp_session_manager import StdioConnectionParams
 from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
 from mcp.client.stdio import StdioServerParameters
 
 
 mcp = MCPToolset(
-    connection_params=StdioServerParameters(
-        command="uvx",
-        args=["mcp-datetime"]
+    connection_params=StdioConnectionParams(
+        server_params=StdioServerParameters(
+            command="uvx",
+            args=["mcp-datetime"],
+        )
     )
 )
