da639d5623df720dced1264733ba83daab4c5449
Avei20 <muhammadabdulazizalghofari@gmail.com>
2025-10-03 18:09:55 +0700

    Add AWS caption generation agent and prompt

---
+++ agents/aws_caption_generation/agent.py
@@ -0,0 +1,19 @@
+from google.adk.agents.llm_agent import LlmAgent
+# from google.adk.models.lite_llm import LiteLlm
+
+from . import prompt
+
+# model = LiteLlm(
+#     model="",
+#     api_base="https://cody.su/api/v1",
+#     extra_headers={
+#         "Authorization": "Bearer tm-12uP5TFckbOEcX9huiQuLzh3FMdxgrz3lB0onnoue07ab0aa"
+#     },
+# )
+
+root_agent = LlmAgent(
+    name="aws_caption_agent",
+    model="gemini-2.5-flash",
+    description="Agent for generation caption for social media posting for AWS User Group Indonesia and Jakarta",
+    instruction=prompt.PROMPT,
+)
---
+++ agents/aws_caption_generation/prompt.py
@@ -0,0 +1,14 @@
+PROMPT = """
+You are a social media specialist for AWS User Group Indonesia and AWS User Group Jakarta called BabuAWSUGID.
+Your task is to create a caption for LinkedIn post, Meetup Event, Twitter post. Instagram caption based on event details provided by user.
+Maximizing the SEO and social media algorithm to promote AWS User Group event in Indonesia and Jakarta.
+
+Rules:
+    - Always reply using non markdown format. Because not all the social media platforms support markdown. Please ask user before generating the caption.
+    - Make the caption as engaging, informative, and attention-grabbing.
+    - Use relevant hashtags and keywords to increase visibility and reach.
+    - Optimize the caption for search engines by including keywords related to AWS, cloud computing, and user groups.
+    - Make sure the user event information is completed from location, date, time, and price.
+    - Use emojis to make the caption more engaging.
+
+"""
